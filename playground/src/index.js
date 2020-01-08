0b55e1ae6e820c309b6fafcd92c9981214cd7324
Jenny Lei <jenny.h.lei@hotmail.com>
2020-01-08 00:32:57 -0500

    lightness stack -- static value

--- playground/src/index.js
+++ playground/src/index.js
@@ -5,23 +5,26 @@ import './index.css';
 import { AlphaPicker, BlockPicker, ChromePicker, CirclePicker, CompactPicker, GithubPicker, HuePicker, MaterialPicker, PhotoshopPicker, SketchPicker, SliderPicker, SwatchesPicker, TwitterPicker, } from 'react-color'
 
 class Component extends React.Component {
-	state = {
-		color: {
-			hex: '#333333',
-			rgb: {
-				r: 51,
-				g: 51,
-				b: 51,
-				a: 1,
+	constructor(props) {
+		super(props);
+		this.state = {
+			color: {
+				hex: '#333333',
+				rgb: {
+					r: 51,
+					g: 51,
+					b: 51,
+					a: 1,
+				},
+				hsl: {
+					h: 0,
+					s: 0,
+					l: .20,
+					a: 1,
+				},
 			},
-			hsl: {
-				h: 0,
-				s: 0,
-				l: .20,
-				a: 1,
-			},
-		},
-	};
+		};
+	}
 
 	// handleChangeComplete = (color, event) => {
 	// 	this.handleChange(color, event);
@@ -37,8 +40,12 @@ class Component extends React.Component {
 		})
 	};
 
-	round = (n) => {
-		return Number((n).toFixed(1));
+	round = (n, i = 1) => {
+		return Number((n).toFixed(i));
+	}
+
+	lightness = () => {
+		return this.round(this.state.color.hsl.l * 100, 0);
 	}
 
 	render() {
@@ -49,6 +56,11 @@ class Component extends React.Component {
 				},
 				noBg: {
 					background: `rgba(0, 0, 0, 0)`,
+				},
+				text: {
+					color: 'black',
+					margin: 0,
+					padding: '10px 0',
 				}
 			},
 		});
@@ -73,9 +85,110 @@ class Component extends React.Component {
 						</div>
 					</div>
 					<div className='bot-half'>
+						<div className='stack-wrapper'>
+							<p style={ styles.text }>LIGHTNESS</p>
 
+							<LightBar
+								fill = { this.lightness() % 10 != 0 }
+								color = { this.state.color.hsl }
+							/>
+						</div>
 					</div>
 				</div>
+			</div>
+		);
+	}
+}
+
+class Rectangle extends React.Component {
+	render() {
+		const styles = reactCSS({
+			'default': {
+				color: {
+					background: `hsla(${ this.props.h * 100 }, ${ this.props.s * 100 }%, ${ this.props.l * 100 }%, ${this.props.a})`,
+				}
+			},
+		});
+		return (
+			<tr className='square-wrapper'>
+				<td className='table-cell'>
+					{this.props.l * 100} %
+				</td>
+				<td
+					className="square"
+					style={styles.color}
+				>
+				</td>
+				<td className='table-cell'>
+					{this.props.l}
+				</td>
+			</tr>
+		);
+	}
+}
+
+class LightBar extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			colors: {
+				h: Array(12).fill(this.props.color.h),
+				s: Array(12).fill(this.props.color.s),
+				l: this.fillArr( Array(12).fill(this.props.color.l) ),
+				a: Array(12).fill(this.props.color.a),
+			}
+		};
+	};
+
+	fillArr(lst) {
+		const _colors = lst.slice();
+
+		for (let i = 0; i < 11; i++) {
+			_colors[i] = i/10;
+		}
+
+		if (this.props.fill) {
+			_colors[11] = this.props.color;
+		} else {
+			_colors[11] = 1000;
+		}
+
+		return _colors;
+	}
+
+	renderRect(i) {
+		console.log((this.state.colors.h[i]));
+		return (
+			<Rectangle
+				h = {this.state.colors.h[i]}
+				s = {this.state.colors.s[i]}
+				l = {this.state.colors.l[i]}
+				a = {this.state.colors.a[i]}
+			/>
+		);
+	}
+
+	render() {
+		if (!this.state.colors.h || !this.state.colors.s || !this.state.colors.l) {
+			return null;
+		}
+
+		return(
+			<div className='swatch-wrapper'>
+				<table>
+					{ this.renderRect( 0 ) }
+					{ this.renderRect( 1 ) }
+					{ this.renderRect( 2 ) }
+					{ this.renderRect( 3 ) }
+					{ this.renderRect( 4 ) }
+					{ this.renderRect( 5 ) }
+					{ this.renderRect( 6 ) }
+					{ this.renderRect( 7 ) }
+					{ this.renderRect( 8 ) }
+					{ this.renderRect( 9 ) }
+					{ this.renderRect( 10 ) }
+					{ this.props.fill ? this.renderRect( 11 ) : null }
+				</table>
 			</div>
 		);
 	}
