11dc7efb7c4718fff2ea27909b356270540952a4
Jenny Lei <jenny.h.lei@hotmail.com>
2020-01-07 21:54:27 -0500

    UI changes

--- playground/src/index.js
+++ playground/src/index.js
@@ -7,7 +7,7 @@ import { AlphaPicker, BlockPicker, ChromePicker, CirclePicker, CompactPicker, Gi
 class Component extends React.Component {
 	state = {
 		color: {
-			hex: '#333',
+			hex: '#333333',
 			rgb: {
 				r: 51,
 				g: 51,
@@ -37,11 +37,15 @@ class Component extends React.Component {
 		})
 	};
 
+	round = (n) => {
+		return Number((n).toFixed(1));
+	}
+
 	render() {
 		const styles = reactCSS({
 			'default': {
 				color: {
-					background: `rgba(${ this.state.color.rgb.r }, ${ this.state.color.rgb.g }, ${ this.state.color.rgb.b }, ${ this.state.color.rgb.a })`,
+					background: `rgba(${ this.state.color.rgb.r }, ${ this.state.color.rgb.g }, ${ this.state.color.rgb.b }, ${ this.state.color.rgb.a})`,
 				},
 				noBg: {
 					background: `rgba(0, 0, 0, 0)`,
@@ -50,10 +54,27 @@ class Component extends React.Component {
 		});
 		return(
 			<div className='wrapper'>
-				<div className='background' style={ styles.color } />
-				<div className='picker'>
+				<div className='background' style={ styles.color }>
+				</div>
+				<div className='picker-wrapper'>
+					<div className='picker'>
 					<ChromePicker className='pickerBlock' color={ this.state.color.rgb } onChange={ this.handleChange } />
-					<p> Chrome </p>
+					<div className='text'> Chrome </div>
+					</div>
+				</div>
+				<div className='content-wrapper'>
+					<div className='top-half'>
+						<h1 className='title'>Colour Picker</h1>
+
+						<div className='code-wrapper'>
+							<p>Hex: {this.state.color.hex}</p>
+							<p>RGB: rgba({this.state.color.rgb.r}, {this.state.color.rgb.g}, {this.state.color.rgb.b}, {this.state.color.rgb.a})</p>
+							<p>HSL: hsla({this.round((this.state.color.hsl.h * 100))}%, {this.round((this.state.color.hsl.s * 100))}%, {this.round((this.state.color.hsl.l * 100))}%, {this.state.color.hsl.a})</p>
+						</div>
+					</div>
+					<div className='bot-half'>
+
+					</div>
 				</div>
 			</div>
 		);
